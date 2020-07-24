ade539b2d782290c34cab3a332ed1d388cb42a83
sakshi sharma <sakshiss.7699@gmail.com>
2020-07-24 12:02:08 +0530

    Code Optimization

--- src/test/java/com/lattice/TestFileUpload/TestFileUploadApplicationTests.java
+++ src/test/java/com/lattice/TestFileUpload/TestFileUploadApplicationTests.java
@@ -1,6 +1,5 @@
 package com.lattice.TestFileUpload;
 
-import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -16,9 +15,7 @@ import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.util.Arrays;
@@ -32,65 +29,57 @@ class TestFileUploadApplicationTests {
 
     @Test
     public void uploadFileInParts() throws IOException, URISyntaxException, JSONException {
+        File file = new File("C:\\Users\\sakshi\\Videos\\Frozen.2013.1080p.BluRay.x264.YIFY.mp4");
 
-        long time = System.currentTimeMillis();
-       File file = new File("C:\\Users\\Sakshi\\Videos\\Goblin\\Gob2.mp4");
-     	//	File file = new File("C:\\Users\\Sakshi\\Videos\\Frozen (2013) [1080p]\\Frozen.2013.1080p.BluRay.x264.YIFY.mp4");
-
-        byte[] fileContent = Files.readAllBytes(file.toPath());
         CloseableHttpClient client = HttpClients.createDefault();
 
-        URIBuilder uribuilder = new URIBuilder("http://localhost:8090/Sangath/forms/files/fileExists");
+        URIBuilder uribuilder = new URIBuilder("http://localhost:8080/fileExists");
         uribuilder.setParameter("filename", file.getName()).setParameter("fileSize", Long.toString(file.length()));
+        System.out.println(file.length());
 
-        HttpGet post = new HttpGet(uribuilder.build());
-        CloseableHttpResponse fileSizeResponse = client.execute(post);
+        HttpGet httpGet = new HttpGet(uribuilder.build());
+        CloseableHttpResponse fileSizeResponse = client.execute(httpGet);
         long bytesWritten = 0;
         String fileResponseString = new BasicResponseHandler().handleResponse(fileSizeResponse);
         System.out.println(fileResponseString);
-        if(fileSizeResponse.getStatusLine().getStatusCode() == 206) {
-
+        if (fileSizeResponse.getStatusLine().getStatusCode() == 206) {
+            System.out.println("Partial Content already exists on server. Initiating partial Upload!!");
             JSONObject json = new JSONObject(fileResponseString);
             bytesWritten = (Integer) json.get("bytes");
         }
         // check if file already exists on server
 
-        if(fileSizeResponse.getStatusLine().getStatusCode() != 200) {
+        if (fileSizeResponse.getStatusLine().getStatusCode() != 200) {
 
-            HttpPost httpPost = new HttpPost("http://localhost:8090/Sangath/forms/files/inPartsUploadStream");
+            HttpPost httpPost = new HttpPost("http://localhost:8080/fileUpload");
             MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-            ByteArrayInputStream inputStream = new ByteArrayInputStream(fileContent);
+            InputStream inputStream = new FileInputStream(file);
 
             if (bytesWritten != 0) {
-                System.out.println("Number of bytes Skipped");
+                System.out.print("Number of bytes Skipped: ");
                 System.out.println(inputStream.skip(bytesWritten));
             }
             builder.addBinaryBody("file", inputStream, ContentType.APPLICATION_OCTET_STREAM
-                    , "Gob2.mp4");
-//           		builder.addBinaryBody("file", inputStream, ContentType.APPLICATION_OCTET_STREAM
-//				, "Frozen.2013.1080p.BluRay.x264.YIFY.mp4");
+                    , "Frozen.2013.1080p.BluRay.x264.YIFY.mp4");
 
-
-//		File file = new File("C:\\Users\\Sakshi\\Videos\\Frozen (2013) [1080p]\\Frozen.2013.1080p.BluRay.x264.YIFY.mp4");
-//		builder.addBinaryBody("file", file, ContentType.APPLICATION_OCTET_STREAM
-//				, "Frozen.2013.1080p.BluRay.x264.YIFY.mp4");
-
-            HttpEntity multipart = builder.build();
 
             // Progress listener - updates task's progress
             BytesTransferred bytesTransferred = new BytesTransferred();
             ProgressEntityWrapper.ProgressListener progressListener =
                     progress -> {
-                        System.out.println(progress);
+                        System.out.println((progress*100)/file.length() + "%" );
                         bytesTransferred.setBytesTransferred(progress);
                     };
 
-            httpPost.setEntity(new ProgressEntityWrapper(multipart, progressListener));
+            httpPost.setEntity(new ProgressEntityWrapper(builder.build(), progressListener));
             httpPost.addHeader("filename", file.getName());
             httpPost.addHeader("fileSize", Long.toString(file.length()));
             CloseableHttpResponse response = null;
+            System.out.println("Starting File Upload!!");
             try {
+                long time = System.currentTimeMillis();
                 response = client.execute(httpPost);
+                System.out.println("Time Taken :" + (System.currentTimeMillis() - time));
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
@@ -98,20 +87,18 @@ class TestFileUploadApplicationTests {
 
                 System.out.println(response.getStatusLine().getStatusCode());
                 System.out.println("Number of bytes transferred: " + bytesTransferred.getBytesTransferred());
-                	assert response.getStatusLine().getStatusCode() == 200;
+                assert response.getStatusLine().getStatusCode() == 200;
                 System.out.println("Accessing content range");
                 System.out.println(Arrays.toString(response.getHeaders("Content-Range")));
                 Long contentRange = Long.parseLong(response.getFirstHeader("Content-Range").getValue());
-                System.out.println("Converted value "  +  contentRange);
+                System.out.println("Converted value " + contentRange);
                 System.out.println("Accessing builder response handler");
                 String responseString = new BasicResponseHandler().handleResponse(response);
                 System.out.println(responseString);
                 client.close();
-                System.out.println("Time Taken :" + (System.currentTimeMillis() - time));
-            }
-        }
-        else {
 
+            }
+        } else {
             System.out.println("Complete file exists on server");
         }
     }
